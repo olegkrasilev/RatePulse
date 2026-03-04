0a5e887776b562a15c8eb2b6348027a8514d2554
olegkrasilev@gmail.com <olegkrasilev@gmail.com>
2026-03-04 11:12:10 +0300

    "feat(core): add configuration module"

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './modules/health/health.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    HealthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
