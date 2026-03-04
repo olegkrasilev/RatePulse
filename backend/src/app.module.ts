9879dbc89a8dae4cf1884990a20ad8e99f5746b6
olegkrasilev@gmail.com <olegkrasilev@gmail.com>
2026-03-04 11:17:15 +0300

    feat(config): add environment validation using Joi

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './modules/health/health.module';
 import { ConfigModule } from '@nestjs/config';
+import { envValidationSchema } from './config/env.validation';
 
 @Module({
   imports: [
     HealthModule,
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: envValidationSchema,
     }),
   ],
   controllers: [],
