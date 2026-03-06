ff7223152f2f300764a49ebeabbde68ba53eb6b5
olegkrasilev@gmail.com <olegkrasilev@gmail.com>
2026-03-06 15:26:15 +0300

    refactor(users): use create user dto in controller

--- backend/src/modules/user/users.controller.ts
+++ backend/src/modules/user/users.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async createUser(@Body() body: { name: string; email: string }) {
-    return this.usersService.createUser(body.name, body.email);
+  async createUser(@Body() dto: CreateUserDto): Promise<User> {
+    return this.usersService.createUser(dto.name, dto.email);
   }
 }
