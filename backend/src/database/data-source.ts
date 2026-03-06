814923100d492b2a85c92d8a75ae964b00a7cfaa
olegkrasilev@gmail.com <olegkrasilev@gmail.com>
2026-03-06 14:56:08 +0300

    feat(db): add TypeORM data source for migrations

---
+++ backend/src/database/data-source.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import { User } from 'src/modules/user/user.entity';
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST ?? 'localhost',
+  port: Number(process.env.DB_PORT ?? 5432),
+  username: process.env.DB_USER ?? 'postgres',
+  password: process.env.DB_PASSWORD ?? 'postgres',
+  database: process.env.DB_NAME ?? 'ratepulse',
+  entities: [User],
+  migrations: ['src/database/migrations/*{.ts,.js}'],
+  synchronize: false,
+});
