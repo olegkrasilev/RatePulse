fd76bc290798993145cca91682fbb4c7a858ef71
olegkrasilev@gmail.com <olegkrasilev@gmail.com>
2026-03-06 10:44:50 +0300

    feat(http): add helmet cors compression

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import helmet from 'helmet';
+import compression from 'compression';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = app.get(Logger);
   app.useLogger(logger);
+  app.use(helmet());
+  app.use(cors());
+  app.use(compression());
 
   app.useGlobalPipes(
     new ValidationPipe({
