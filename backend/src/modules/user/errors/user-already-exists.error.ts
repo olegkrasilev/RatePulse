244f7273ef0f3e8e35e0fbcf094a118a2435edd3
olegkrasilev@gmail.com <olegkrasilev@gmail.com>
2026-03-09 14:34:38 +0300

    feat: refactor error messages

--- backend/src/modules/user/errors/user-already-exists.error.ts
+++ backend/src/modules/user/errors/user-already-exists.error.ts
@@ -1,5 +1,5 @@
-import { ERROR_MESSAGES } from 'src/common/constants/errors/error-messages';
-import { ERROR_CODES } from 'src/common/constants/errors/errors';
+import { ERROR_MESSAGES } from 'src/common/errors/error-messages';
+import { ERROR_CODES } from 'src/common/errors/errors';
 
 export class UserAlreadyExistsError extends Error {
   public readonly code = ERROR_CODES.USER_ALREADY_EXISTS;
