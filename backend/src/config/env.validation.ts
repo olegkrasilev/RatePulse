d48dc578e1c4cd981e36d74c63ad2a48706714b8
olegkrasilev@gmail.com <olegkrasilev@gmail.com>
2026-03-09 14:00:29 +0300

    feat(config): validate database environment variables

--- backend/src/config/env.validation.ts
+++ backend/src/config/env.validation.ts
@@ -1,9 +1,15 @@
 import * as Joi from 'joi';
 
 export const envValidationSchema = Joi.object({
+  // APP
   NODE_ENV: Joi.string()
     .valid('development', 'production', 'test')
     .default('development'),
-
   PORT: Joi.number().default(3000),
+  // DATABASE
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().required(),
+  DB_USER: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
 });
