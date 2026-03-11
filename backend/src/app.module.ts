792245fcf35a135c714ad5f28011bfeea27d0816
olegkrasilev@gmail.com <olegkrasilev@gmail.com>
2026-03-11 12:41:17 +0300

    feat(security): enable global throttler guard

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,7 @@ import { HealthModule } from './modules/health/health.module';
 import { ConfigModule } from '@nestjs/config';
 import { envValidationSchema } from './config/env.validation';
 import { LoggerModule } from 'nestjs-pino';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { CatchEverythingFilter } from './common/filters/catch-everything-filter';
 import { RequestIdMiddleware } from './common/middleware/request-id.middleware';
@@ -11,7 +11,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/user/users.module';
 import { databaseConfig } from './database/database.config';
 import { TypeOrmExceptionFilter } from './common/filters/typeorm-exception.filter';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -54,6 +54,10 @@ import { ThrottlerModule } from '@nestjs/throttler';
     {
       provide: APP_FILTER,
       useClass: TypeOrmExceptionFilter,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
   ],
 })
