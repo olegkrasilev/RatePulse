daa8b58ca676b78a98c8302a3a804ee6b9aa580b
olegkrasilev@gmail.com <olegkrasilev@gmail.com>
2026-03-30 11:20:36 +0300

    feat: (env)

--- digital-bank-core/src/app.module.ts
+++ digital-bank-core/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app/app.config';
+import databaseConfig from './config/database/database.config';
+import { envValidationSchema } from './config/validation/env.validation';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, databaseConfig],
+      validationSchema: envValidationSchema,
+      cache: true,
+    }),
+  ],
 })
 export class AppModule {}
