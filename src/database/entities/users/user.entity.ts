234deb7979971c8ed9905e5c666b2c7e7571a000
olegkrasilev@gmail.com <olegkrasilev@gmail.com>
2026-03-30 16:37:47 +0300

    fix: file structure

--- src/database/entities/users/user.entity.ts
+++ src/database/entities/users/user.entity.ts
@@ -1,6 +1,6 @@
 import { Entity, Column } from 'typeorm';
 
-import { BaseEntity } from '../../common/entities/base.entity';
+import { BaseEntity } from '../base.entity';
 
 @Entity('users')
 export class User extends BaseEntity {
